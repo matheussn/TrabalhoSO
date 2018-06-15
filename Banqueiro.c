434ba86b5ea3e397563399f4511622ca33b0c0df
mbosprates <40080058+mbosprates@users.noreply.github.com>
2018-06-15 09:08:19 -0300

    Update Banqueiro.c

--- Banqueiro.c
+++ Banqueiro.c
@@ -4,7 +4,35 @@
 #include "Dados.h"
 
 int requisicao_recursos(int pid, int recursos[]){
+    
+    int i, j;
     int x;
+ /*   
+    int * flag = (int *) malloc (nro_processo * sizeof(int));
+        for(i = 0; i < nro_processo; i++)
+            flag[i] = 0;
+    
+
+    for(i = 0; i < dados->nro_processo; i++)
+    {
+        for(j = 0; j< dados->nro_recurso; j++)
+        {
+            if (dados->processos[i].quantNecess[j] < recursos[j])
+                flag[i]++;        
+        }       
+    }
+
+    for(i = 0; i<nro_processo; i++)
+        if(flag[i] == nro_recurso)
+        {
+            // estado seguro, é possivel alocar.
+            return -1;   
+        }
+    
+    
+    //Não foi possivel alocar.
+*/
+
 	srand( (unsigned)time(NULL));
 
 	x =  ( rand() % 10 );
